2c180f085da3a31e11b3a37c9dfe9c8c1866fb0b
Luca Casa <lc301092@gmail.com>
2019-03-25 18:36:16 +0100

    comments on tasks to be done
    In range.js
    - consider changing name

--- JS/range_value.js
+++ JS/range_value.js
@@ -1,13 +1,32 @@
 $(document).ready(function () {
 
-    $("[type=range]").change(function () {
-        var newValue = $(this).val();
-        $(this).next().text(newValue);
-    });
+	$("[type=range]").change(function () {
+		var newValue = $(this).val();
+		$(this).next().text(newValue);
+	});
 });
 
 
 function updateSpacing() {
-    var spacing = $("#p1").val();
-    $("#text_area").css("word-spacing", 5 * spacing)
+	var spacing = $("#p1").val();
+	$("#text_area").css("word-spacing", 5 * spacing)
 };
+
+function updateParaghs() {
+	// get all text (use textfield id and value)
+	// count words or end of sentences.
+	// get slider value
+
+}
+
+function updateFont() {
+	// get the selected font
+	// set the new font on the text field.
+	
+}
+
+function updateDifficulty() {
+	// Wait with thise one  
+	// Here should call the algorith which will return some information
+
+}
