bfbd3d023fceda7f2928607afe7eaac7fe6a9f85
Miriam <miriamkrebs606@gmail.com>
2019-03-26 13:11:08 +0100

    font type
    Change font-type through slider

--- JS/range_value.js
+++ JS/range_value.js
@@ -1,38 +1,42 @@
 const synoWords = {
-		ændringen: ["tænke", ""],
-		predefineret: ["fastlagt","forbestemt"],
-		synonymer: ["ens","samme","lignende","ensbetydende"]
-		};
+    ændringen: ["tænke", ""],
+    predefineret: ["fastlagt", "forbestemt"],
+    synonymer: ["ens", "samme", "lignende", "ensbetydende"]
+};
 
 $(document).ready(function () {
 
-	$("[type=range]").change(function () {
-		var newValue = $(this).val();
-		$(this).next().text(newValue);
-	});
+    $("[type=range]").change(function () {
+        var newValue = $(this).val();
+        $(this).next().text(newValue);
+    });
 });
 
 
 function updateSpacing() {
-	var spacing = $("#p1").val();
-	$("#text_area").css("word-spacing", 5 * spacing)
+    var spacing = $("#p1").val();
+    $("#text_area").css("word-spacing", 5 * spacing)
 };
 
 function updateParagshs() {
-	// get all text (use textfield id and value)
-	// count words or end of sentences.
-	// get slider value
+    // get all text (use textfield id and value)
+    // count words or end of sentences.
+    // get slider value
 
 }
 
 function updateFont() {
-	// get the selected font
-	// set the new font on the text field.
+    var font = $("#p2").val();
+    var fontType = ['opendyslexic', 'Times New Roman', 'Arial']
+
+    $("#text_area").css("font-family", fontType[font]);
+    // get the selected font
+    // set the new font on the text field.
 
 }
 
 function updateDifficulty() {
-	// Here should call the algorithm which will return some information
-	// for now we will use synoWords which is a hardcoded variable.
+    // Here should call the algorithm which will return some information
+    // for now we will use synoWords which is a hardcoded variable.
 
 }
